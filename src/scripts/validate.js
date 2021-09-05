1b8b5abab51e9bdc74c1cdb662047587737ce213
GreatGameDota <17858650+GreatGameDota@users.noreply.github.com>
2021-09-04 20:20:01 -0500

    Fix bug with input

--- src/scripts/validate.js
+++ src/scripts/validate.js
@@ -7,23 +7,43 @@ const validate = (type, input, bits) => {
 		// Binary
 		if (input.substring(0, 2) === '0b') input = input.substring(2, input.length);
 		if (input === '') errors.push([ true, "Input can't be empty" ]);
-		else if (/^[01.]+$/.test(input)) errors.push([ false, '' ]);
-		else errors.push([ true, 'Invalid binary input' ]);
+		else if (/^[01.]+$/.test(input)) {
+			if (input.replace(/[^.]/g, '').length <= 1) {
+				errors.push([ false, '' ]);
+			} else {
+				errors.push([ true, 'Invalid binary input' ]);
+			}
+		} else errors.push([ true, 'Invalid binary input' ]);
 	} else if (type === 1) {
 		// Decimal
-		if (/^[-0123456789.]+$/.test(input)) errors.push([ false, '' ]);
-		else errors.push([ true, 'Invalid decimal input' ]);
+		if (/^[-0123456789.]+$/.test(input)) {
+			if (input.replace(/[^-]/g, '').length <= 1 && input.replace(/[^.]/g, '').length <= 1) {
+				if (input.replace(/[^-]/g, '').length === 1) {
+					if (input[0] === '-') errors.push([ false, '' ]);
+					else errors.push([ true, 'Invalid decimal input' ]);
+				} else {
+					errors.push([ false, '' ]);
+				}
+			} else {
+				errors.push([ true, 'Invalid decimal input' ]);
+			}
+		} else errors.push([ true, 'Invalid decimal input' ]);
 	} else if (type === 2) {
 		// Hexadecimal
 		if (input.substring(0, 2) === '0x') input = input.substring(2, input.length);
 		input = input.toUpperCase();
 		if (input === '') errors.push([ true, "Input can't be empty" ]);
-		else if (/^[0123456789ABCDEF.]+$/.test(input)) errors.push([ false, '' ]);
-		else errors.push([ true, 'Invalid hexadecimal input' ]);
+		else if (/^[0123456789ABCDEF.]+$/.test(input)) {
+			if (input.replace(/[^.]/g, '').length <= 1) {
+				errors.push([ false, '' ]);
+			} else {
+				errors.push([ true, 'Invalid hexadecimal input' ]);
+			}
+		} else errors.push([ true, 'Invalid hexadecimal input' ]);
 	}
 
-    if (bits <= 0) errors.push([true, 'Invalid amount of bits']);
-    else if (!/^[0123456789]+$/.test(bits)) errors.push([true, 'Invalid bits input']);
+	if (bits <= 0) errors.push([ true, 'Invalid amount of bits' ]);
+	else if (!/^[0123456789]+$/.test(bits)) errors.push([ true, 'Invalid bits input' ]);
 	else errors.push([ false, '' ]);
 
 	return errors;
